cd8faf629e8a9c6f2e90562620fe102b4fa3276d
StandByNet <oukaci.ahcene@hotmail.fr>
2022-06-16 22:11:08 +0200

    Tdd For BasketBall

---
+++ TestTdd_Basket/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestTdd_Basket
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetScore_return000_000_when_TeamAGetOneGool()
+        {
+            //Expected
+            //Actuel
+            //Assert
+            //Assert.AreEqual(,)
+        }
+    }
+}
