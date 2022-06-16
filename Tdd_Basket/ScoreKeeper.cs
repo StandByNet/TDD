b1844fc07767da28d23d1afcf2acd84e0a1e92e0
StandByNet <oukaci.ahcene@hotmail.fr>
2022-06-17 00:31:55 +0200

    coding GetScore with TDD

---
+++ Tdd_Basket/ScoreKeeper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tdd_Basket
+{
+    public class ScoreKeeper : IScoreKeeper
+    {
+        private string scoreBorad = "";
+        private string scoreTeamA = "";
+        private string scoreTeamB = "";
+        private int scoreTeam1 = 0;
+        private int scoreTeam2 = 0;
+
+        public string GetScore()
+        {
+            if (scoreBorad.Equals(scoreTeamA + ":" + scoreTeamB))
+            {
+                scoreTeam1++;
+                scoreTeamA = ConvertScore(scoreTeam1);// scoreTeam1.ToString();
+                scoreBorad = scoreTeamA + ":" + scoreTeamB;
+                return scoreBorad;
+            }
+            scoreTeamA = "000";
+            scoreTeamB = "000";
+            scoreBorad = scoreTeamA + ":" + scoreTeamB;
+            return scoreBorad;
+        }
+
+        private string ConvertScore(int scoreTeam1)
+        {
+            return scoreTeam1.ToString().Length switch
+            {
+                1 => "00" + scoreTeam1.ToString(),
+                2 => "0" + scoreTeam1.ToString(),
+                3 => scoreTeam1.ToString(),
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        public void ScoreTeamA1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScoreTeamA2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScoreTeamA3()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScoreTeamB1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScoreTeamB2()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ScoreTeamB3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestTdd_Basket/UnitTest1.cs
+++ TestTdd_Basket/UnitTest1.cs
@@ -1,17 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tdd_Basket;
 
 namespace TestTdd_Basket
 {
     [TestClass]
     public class UnitTest1
     {
+        readonly ScoreKeeper scoreKeeper = new();
+
         [TestMethod]
-        public void GetScore_return000_000_when_TeamAGetOneGool()
+        public void GetScore_return000_000_when_ScoreBegining()
         {
-            //Expected
-            //Actuel
-            //Assert
-            //Assert.AreEqual(,)
+            Assert.AreEqual("000:000", scoreKeeper.GetScore());
+        }
+
+        [TestMethod]
+        public void GetScore_return001_000_when_TeamAGetOneGool()
+        {
+            Assert.AreEqual("000:000", scoreKeeper.GetScore());
+            Assert.AreEqual("001:000", scoreKeeper.GetScore());
+            Assert.AreEqual("002:000", scoreKeeper.GetScore());
+            Assert.AreEqual("003:000", scoreKeeper.GetScore());
         }
     }
 }
